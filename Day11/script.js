fc7d97ca718d5db544657524ff91eba10104ca6a
Rohitgupta786 <73888330+Rohitgupta786@users.noreply.github.com>
2023-05-16 20:09:27 +0530

    Add files via upload

---
+++ Day11/script.js
@@ -0,0 +1,17 @@
+document.write("js baisc");
+function myfun(){
+    //console.log("myfun work in consle");
+    //console.log(Math.round(Math.random()*10));
+    var a=Math.round(Math.random()*10);
+    console.log(a);
+    document.getElementById("para").innerHTML=a;
+
+    var b=Math.round(Math.random()*10);
+    console.log(b);
+    document.getElementById("ab").innerHTML=b;
+
+
+
+}
+
+
